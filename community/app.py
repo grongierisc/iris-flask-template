742ca99e766366cbe9f24199774c9d090afba8cf
grongierisc <guillaume.rongier@intersystems.com>
2024-06-19 16:38:12 +0200

    Add Iris query endpoint to retrieve top 10 class definitions

--- community/app.py
+++ community/app.py
@@ -2,10 +2,26 @@
 from flask import Flask, jsonify
 from models import Comment, Post, init_db
 
+import iris
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'iris://SuperUser:SYS@localhost:59942/USER'
 
 db = init_db(app)
+
+######################
+# IRIS Query example #
+######################
+
+@app.route('/iris', methods=['GET'])
+def iris_query():
+    query = "SELECT top 10 * FROM %Dictionary.ClassDefinition"
+    rs = iris.sql.exec(query)
+    # Convert the result to a list of dictionaries
+    result = []
+    for row in rs:
+        result.append(row)
+    return jsonify(result)
 
 ###################
 # CRUD operations #
